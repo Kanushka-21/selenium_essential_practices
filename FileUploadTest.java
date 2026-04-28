5f6d3642f5cc862b5f0ca62ce043c79e1d68757f
Kanushka-21 <iit21062@std.uwu.ac.lk>
2026-04-28 17:49:53 +0530

    updates

--- FileUploadTest.java
+++ FileUploadTest.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class FileUploadTest extends BaseTest {
-    
+
     @Test
     public void testFileUpload() throws Exception {
         // Create test file
@@ -14,29 +14,29 @@ public class FileUploadTest extends BaseTest {
         FileWriter writer = new FileWriter(testFile);
         writer.write("This is a test file for upload");
         writer.close();
-        
+
         // Navigate to upload page
         driver.get("https://the-internet.herokuapp.com/upload");
         Thread.sleep(1000);
-        
+
         // Upload file
         WebElement fileInput = driver.findElement(By.id("file-upload"));
         String filePath = testFile.getAbsolutePath();
         fileInput.sendKeys(filePath);
         Thread.sleep(500);
-        
+
         // Submit form
         driver.findElement(By.id("file-submit")).click();
         Thread.sleep(2000);
-        
+
         // Get upload message
         WebElement uploadMessage = driver.findElement(By.tagName("h3"));
         String messageText = uploadMessage.getText();
-        
+
         // Assert upload success
-        Assert.assertTrue(messageText.contains("File Uploaded"), 
-            "File upload failed!");
-        
+        Assert.assertTrue(messageText.contains("File Uploaded"),
+                "File upload failed!");
+
         // Cleanup
         testFile.delete();
     }
--- TableTest.java
+++ TableTest.java
@@ -5,28 +5,28 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class TableTest extends BaseTest {
-    
+
     @Test
     public void testReadTableData() throws Exception {
         // Navigate to tables page
         driver.get("https://the-internet.herokuapp.com/tables");
         Thread.sleep(1000);
-        
+
         // Find the first table
         WebElement table = driver.findElement(By.id("table1"));
-        
+
         // Find all rows
         List<WebElement> rows = table.findElements(By.tagName("tr"));
-        
+
         // Get first data row (skip header)
         WebElement firstRow = rows.get(1);
         List<WebElement> cells = firstRow.findElements(By.tagName("td"));
-        
+
         // Extract data
         String lastName = cells.get(0).getText();
         String firstName = cells.get(1).getText();
         String email = cells.get(2).getText();
-        
+
         // Assert data exists
         Assert.assertTrue(lastName.length() > 0, "Last name is empty!");
         Assert.assertTrue(firstName.length() > 0, "First name is empty!");
