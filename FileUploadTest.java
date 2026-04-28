fcd3fe096be48bc9ea001bda8bea047ffd2b3866
Kanushka-21 <iit21062@std.uwu.ac.lk>
2026-04-28 17:20:47 +0530

    file upload test automation

--- FileUploadTest.java
+++ FileUploadTest.java
@@ -12,40 +12,40 @@ public class FileUploadTest {
         FileWriter writer = new FileWriter(testFile);
         writer.write("This is a test file for upload");
         writer.close();
-        
+
         // Create driver instance
         WebDriver driver = new ChromeDriver();
-        
+
         // Open file upload page
         driver.get("https://the-internet.herokuapp.com/upload");
         Thread.sleep(1000);
-        
+
         // Find file input element
         WebElement fileInput = driver.findElement(By.id("file-upload"));
-        
+
         // Send file path to input
         String filePath = testFile.getAbsolutePath();
         fileInput.sendKeys(filePath);
         Thread.sleep(500);
-        
+
         // Find and click submit button
         driver.findElement(By.id("file-submit")).click();
         Thread.sleep(2000);
-        
+
         // Check if upload successful message appears
         WebElement uploadMessage = driver.findElement(By.tagName("h3"));
         String messageText = uploadMessage.getText();
-        
+
         // Verify success
         if (messageText.contains("File Uploaded")) {
             System.out.println("TEST PASSED");
         } else {
             System.out.println("TEST FAILED");
         }
-        
+
         // Delete test file
         testFile.delete();
-        
+
         // Close browser
         driver.quit();
     }
